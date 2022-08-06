c2f58cb68e31fa0a93ce8844dc500912c2e458ad
vaporwave9 <64961250+vaporwave9@users.noreply.github.com>
2022-08-06 12:25:27 +0200

    Update youtube_dark_no_autoplay.user.js

--- youtube_dark_no_autoplay.user.js
+++ youtube_dark_no_autoplay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         YouTube Dark Theme + No Autoplay on Steroids
-// @namespace    vw9YouTubeDarkThemeNoAutoplayRoids
-// @version      1.2.9
+// @name         YouTube Dark Theme + No Autoplay
+// @namespace    vw9YouTubeDarkThemeNoAutoplay
+// @version      1.3.0
 // @description  Dark theme with no autoplay
 // @updateURL    https://github.com/vaporwave9/userscripts-collection/raw/master/youtube_dark_no_autoplay.user.js
 // @downloadURL  https://github.com/vaporwave9/userscripts-collection/raw/master/youtube_dark_no_autoplay.user.js
@@ -11,8 +11,6 @@
 // @run-at       document-start
 // @grant        none
 // @license      WTFPL
-// @require      https://code.jquery.com/jquery-1.7.2.min.js
-// @require      https://raw.githubusercontent.com/vaporwave9/userscripts-collection/master/waitForKeyElements.js
 // @icon         https://raw.githubusercontent.com/vaporwave9/userscripts-collection/master/icon.png
 // @noframes
 // ==/UserScript==
@@ -26,7 +24,6 @@ var fired = 0,
     isDark = false,
     noAutoplay = false,
     isEnglish = false; //do not edit
-
 
 function gensokyo4() {
     var element1 = document.querySelectorAll("[aria-label^='Cancel']");
@@ -56,7 +53,7 @@ function gensokyo3() {
     }
 }
 
-function gensokyo2() {
+function gensokyo() {
     var matchA = document.cookie.match(new RegExp('(^| )' + nameP + '=([^;]+)'));
     if (matchA[2].includes('f5=20000') === true) {
         var newstr = matchA[2].replace(/f5=20000/, "f5=30000");
@@ -84,34 +81,13 @@ function gensokyo2() {
     if (isDark === true && noAutoplay === true && isEnglish == true) {
         console.log(isDark, noAutoplay, isEnglish, "S ALL GOOD MAN!")
     } else {
-        if (/https:\/\/www\.youtube\.com\/watch.*/.test(window.location.href) === false) {
-            location.reload();
-            console.log("reloading!");
-        }
+        location.reload();
+        console.log("reloading!");
         gensokyo3();
     }
 }
 
-function gensokyo() {
-    var element1 = document.querySelectorAll("[aria-label^='Reject']");
-    if (element1 != undefined) {
-        if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === true) {
-            document.cookie = 'PREF=tz=Europe.Berlin&f5=30000&f6=400&gl=US;  path=/; domain=.youtube.com';
-            element1[0].click();
-        }
-        if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === false) {
-            var ragemode = document.querySelector("ytd-app")
-            if (ragemode.isAppDarkTheme() === false) {
-                ragemode.onDarkModeToggledAction()
-            }
-            element1[0].click();
-            gensokyo2();
-        }
-    }
-    return false;
-}
-
-waitForKeyElements("[aria-label^='Reject']", gensokyo);
 window.addEventListener("yt-navigate-finish", gensokyo);
 window.addEventListener("spfdone", gensokyo);
 var refreshIntervalId4 = setInterval(gensokyo4, 400);
+gensokyo();
