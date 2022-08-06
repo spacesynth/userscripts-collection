7c869fecb52e99b9018432909c21c1712ae406bc
vaporwave9 <64961250+vaporwave9@users.noreply.github.com>
2022-08-06 12:26:26 +0200

    Update youtube_rejector.user.js

--- youtube_rejector.user.js
+++ youtube_rejector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Rejector
 // @namespace    vw9YouTubeRejector
-// @version      1.1.3
+// @version      1.3.0
 // @description  No consent data sniffers
 // @author       vaporwave9
 // @match        *://*.youtube.com/*
@@ -15,47 +15,24 @@
 // ==/UserScript==
 
 'use strict';
-var fired = 0;
-function gensokyo3() {
-    var player = document.getElementById("movie_player")
-    if (player != null) {
-        if (document.visibilityState === 'visible' && fired < 1) {
-            if (player.getPlayerState() == 1) {
-                fired += 1;
-                console.log("fired!");
-            } else if (player.getPlayerState() != 1) {
-                player.playVideo();
-            }
-        }
-    }
-}
-
 function gensokyo() {
     var element1 = document.querySelectorAll("[aria-label^='Reject']");
     if (element1 != undefined) {
         if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === true) {
             document.cookie = 'PREF=tz=Europe.Berlin&f5=30000&f6=400&gl=US;  path=/; domain=.youtube.com';
+            element1[0].click();
         }
-        element1[0].click();
+        if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === false) {
+            var ragemode = document.querySelector("ytd-app")
+            if (ragemode.isAppDarkTheme() === false) {
+                //ragemode.onDarkModeToggledAction()
+            }
+            element1[0].click();
+        }
     }
-    fired += 1;
-    gensokyo3()
     return false;
 }
 
-function gensokyo2() {
-    if (fired > 0) {
-        clearInterval(refreshIntervalId);
-    } else {
-        var aTags2 = document.getElementsByTagName("a");
-        for (var j = 0; j < aTags2.length; j++) {
-            if (aTags2[j].textContent == "Reject") {
-                aTags2[j].click();
-                fired += 1;
-            }
-        }
-    }
-    gensokyo3()
-}
 waitForKeyElements("[aria-label^='Reject']", gensokyo);
-var refreshIntervalId = setInterval(gensokyo2, 100);
+window.addEventListener("yt-navigate-finish", gensokyo);
+window.addEventListener("spfdone", gensokyo);
