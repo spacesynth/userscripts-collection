5198b141043d841ee1af493b2190707147fd3553
vaporwave9 <64961250+vaporwave9@users.noreply.github.com>
2023-01-13 18:00:48 +0100

    Update youtube_rejector.user.js

--- youtube_rejector.user.js
+++ youtube_rejector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Rejector
 // @namespace    vw9YouTubeRejector
-// @version      1.4.0
+// @version      1.4.1
 // @description  No consent data sniffers
 // @updateURL    https://github.com/vaporwave9/userscripts-collection/edit/master/youtube_rejector.user.js
 // @downloadURL  https://github.com/vaporwave9/userscripts-collection/edit/master/youtube_rejector.user.js
@@ -19,23 +19,27 @@
 
 'use strict';
 function gensokyo() {
-    var element1 = document.querySelectorAll("[aria-label^='Reject the use of cookies']");
-    if (element1 != undefined) {
-        if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === true) {
-            document.cookie = 'PREF=tz=Europe.Berlin&f5=30000&f6=400&gl=US;  path=/; domain=.youtube.com';
+    if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === true) {
+        var element1 = document.querySelectorAll("[aria-label='Reject all']");
+        document.cookie = 'PREF=tz=Europe.Berlin&f5=30000&f6=400&gl=US;  path=/; domain=.youtube.com';
+        if (element1 != undefined) {
             element1[0].click();
         }
-        if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === false) {
-            var ragemode = document.querySelector("ytd-app")
-            if (ragemode.isAppDarkTheme() === false) {
-                //ragemode.onDarkModeToggledAction()
-            }
-            element1[0].click();
+    }
+    if (/https\:\/\/(?=consent).*\.youtube\..*/.test(window.location.href) === false) {
+        var element2 = document.querySelectorAll("[aria-label^='Reject the use of cookies']");
+        var ragemode = document.querySelector("ytd-app")
+        if (ragemode.isAppDarkTheme() === false) {
+            //ragemode.onDarkModeToggledAction()
+            //document.getElementsByTagName("html")[0].setAttribute("dark", "true");
+        }
+        if (element2 != undefined) {
+            element2[0].click();
         }
     }
     return false;
 }
 
-waitForKeyElements("[aria-label^='Reject the use of cookies']", gensokyo);
+waitForKeyElements("[aria-label^='Reject']", gensokyo);
 window.addEventListener("yt-navigate-finish", gensokyo);
 window.addEventListener("spfdone", gensokyo);
