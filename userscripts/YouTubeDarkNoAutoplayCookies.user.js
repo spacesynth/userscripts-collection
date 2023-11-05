a8b615ee9ab5ba847e7fa9a824b256942e049dd0
spacesynth <64961250+spacesynth@users.noreply.github.com>
2023-11-05 19:51:35 +0100

    Create YouTubeDarkNoAutoplayCookies.user.js

---
+++ userscripts/YouTubeDarkNoAutoplayCookies.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         # YouTube Dark + No Autoplay (Cookies)
+// @namespace    spsYouTubeDarkNoAutoplayCookies
+// @description  Dark theme with no autoplay
+// @version      1.4.0
+// @author       spacesynth
+// @supportURL   https://github.com/spacesynth/userscripts-collection
+// @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
+// @license      WTFPL
+// @match        *://*.youtube.com/*
+// @run-at       document-start
+// @grant        GM_cookie
+// @grant        GM.cookie
+// @noframes
+// ==/UserScript==
+
+(function() {
+'use strict';
+if (/.*PREF.*/.test(document.cookie)===false){
+if (/.*PREF=tz=Europe.Berlin&f5=30000&f6=400&gl=US"/.test(document.cookie)===false){
+GM.cookie.delete({ name: 'PREF' })
+GM_cookie.set(
+    {
+        //domain: "www.youtube.com",
+        hostOnly: true,
+        httpOnly: false,
+        name: "PREF",
+        path: "/",
+        sameSite: "no_restriction",
+        secure: true,
+        session: false,
+        value: "tz=Europe.Berlin&f5=30000&f6=400&gl=US"
+    }
+)
+location.reload();
+}
+}
+console.log('Sanae is no longer winning!')
+})();
