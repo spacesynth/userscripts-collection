be373cc24f85f3c484ddde37efd21fcad758396b
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-03-07 16:15:44 +0000

    Update ImgurNoBeta.user.js

--- userscripts/ImgurNoBeta.user.js
+++ userscripts/ImgurNoBeta.user.js
@@ -2,7 +2,7 @@
 // @name         # Imgur No Beta
 // @namespace    spsImgurNoBeta
 // @description  Activate classic Imgur design
-// @version      1.0.0
+// @version      1.0.1
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
@@ -16,13 +16,15 @@
 'use strict';
 function cookieMaker() {
     if (document.cookie.includes('frontpagebetav2=0') == false) {
-        var a = "frontpagebetav2=0;";
-        var b = a.split(";");
-        for (var c = 0; c < b.length; c++) {
-            document.cookie = b[c] + "; path=/";
+        if (document.cookie.includes('postpagebeta=0') == false) {
+            var a = "frontpagebetav2=0;postpagebeta=0";
+            var b = a.split(";");
+            for (var c = 0; c < b.length; c++) {
+                document.cookie = b[c] + "; path=/";
+            }
+            location.reload();
+            console.log("this only runs once per session");
         }
-        location.reload();
-        console.log("this only runs once per session");
     }
 }
 cookieMaker();
