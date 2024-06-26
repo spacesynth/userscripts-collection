0b97f468b99021c15f9aa4a633d2215204d16b76
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-06-26 11:08:03 +0200

    Update YouTubeNoAutoplayChannel.user.js

--- userscripts/YouTubeNoAutoplayChannel.user.js
+++ userscripts/YouTubeNoAutoplayChannel.user.js
@@ -2,12 +2,14 @@
 // @name         # YouTube No Autoplay (Channel)
 // @namespace    spsYouTubeNoAutoplayChannel
 // @description  No autoplay on channels
-// @version      1.0.0
+// @version      1.0.1
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
 // @license      WTFPL
-// @match        *://*.youtube.com/*
+// @match        *://*.youtube.com/channel*
+// @match        *://*.youtube.com/@*
+// @exclude      *://*.youtube.com/watch*
 // @run-at       document-start
 // @grant        none
 // @noframes
@@ -15,29 +17,15 @@
 
 'use strict';
 function gensokyo4() {
-    if (/https\:\/\/www\.youtube\.com\/channel\/.*/.test(window.location.href) === true) {
-        var element1 = document.querySelector('ytd-player').getPlayer();
-        var alternative1 = document.querySelector('ytd-browse video');
-        if (element1 != undefined) {
-            element1.pauseVideo();
-        }
-        if (alternative1 != undefined) {
-            alternative1.pause();
-        }
+    var element1 = document.querySelector('ytd-player').getPlayer();
+    var alternative1 = document.querySelector('ytd-browse video');
+    if (element1 != undefined) {
+        element1.pauseVideo();
     }
-
-    if (/https\:\/\/www\.youtube\.com\/@.*/.test(window.location.href) === true) {
-        var element2 = document.querySelector('ytd-player').getPlayer();
-        var alternative2 = document.querySelector('ytd-browse video');
-        if (element2 != undefined) {
-            element2.pauseVideo();
-        }
-        if (alternative2 != undefined) {
-            alternative2.pause();
-        }
+    if (alternative1 != undefined) {
+        alternative1.pause();
     }
 }
-
 var refreshIntervalId4 = setInterval(gensokyo4, 400);
 window.addEventListener("yt-navigate-finish", gensokyo4);
 window.addEventListener("spfdone", gensokyo4);
