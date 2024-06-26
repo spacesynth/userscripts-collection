305777ca40f59c0cdff6c811dfb5d92973853ef8
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-06-26 20:02:45 +0200

    Update GoogleAutoDisableSafeSearch.user.js

--- userscripts/GoogleAutoDisableSafeSearch.user.js
+++ userscripts/GoogleAutoDisableSafeSearch.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name         # Google Auto Disable SafeSearch
+// @name         # Google Auto Disable SafeSearch & Cookies
 // @namespace    spsGoogleAutoDisableSafeSearch
 // @description  Screw Safe Search
-// @version      1.0.0
+// @version      1.0.1
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
 // @license      WTFPL
-// @include      http*://*.google.*/search*tbm=isch*
+// @include      http*://www.google.*/*
+// @exclude      http*://www.google.*/maps*
+// @exclude      http*://www.google.*/search*
 // @require      https://code.jquery.com/jquery-1.7.2.min.js
 // @require      https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/waitForKeyElements.js
 // @run-at       document-start
@@ -17,7 +19,24 @@
 
 'use strict';
 var url = window.location.href;
+var a;
+
+//user consent blocked cookies blocked
+if (url.indexOf("ucbcb=1") == -1) {
 if (url.indexOf("safe=off") == -1) {
-        url += "&safe=off";
-        window.location = url;
+a = url.split("com/");
+url = a[0] + "com/?safe=off&ucbcb=1&" + a[1].replace('\?','');
+window.location = url;
+}}
+
+if (url.indexOf("ucbcb=1") == -1) {
+a = url.split("com/");
+url = a[0] + "com/?ucbcb=1&" + a[1].replace('\?','');
+window.location = url;
+}
+
+if (url.indexOf("safe=off") == -1) {
+a = url.split("com/");
+url = a[0] + "com/?safe=off&" + a[1].replace('\?','');
+window.location = url;
 }
