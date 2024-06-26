532ef3c5b18c99df21e8bb3420c2221b96ef3b77
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-06-26 18:42:44 +0200

    Update YouTubeNoAutoplayChannel.user.js

--- userscripts/YouTubeNoAutoplayChannel.user.js
+++ userscripts/YouTubeNoAutoplayChannel.user.js
@@ -2,7 +2,7 @@
 // @name         # YouTube No Autoplay (Channel)
 // @namespace    spsYouTubeNoAutoplayChannel
 // @description  No autoplay on channels
-// @version      1.0.1
+// @version      1.0.2
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 'use strict';
-function gensokyo4() {
+function gensokyo6() {
     var element1 = document.querySelector('ytd-player').getPlayer();
     var alternative1 = document.querySelector('ytd-browse video');
     if (element1 != undefined) {
@@ -26,6 +26,15 @@ function gensokyo4() {
         alternative1.pause();
     }
 }
-var refreshIntervalId4 = setInterval(gensokyo4, 400);
+function gensokyo4() {
+    if (/https\:\/\/www\.youtube\.com\/channel\/.*/.test(window.location.href) === true) {
+        gensokyo6();
+    }
+
+    if (/https\:\/\/www\.youtube\.com\/@.*/.test(window.location.href) === true) {
+        gensokyo6();
+    }
+}
+var refreshIntervalId4 = setInterval(gensokyo4, 200);
 window.addEventListener("yt-navigate-finish", gensokyo4);
 window.addEventListener("spfdone", gensokyo4);
