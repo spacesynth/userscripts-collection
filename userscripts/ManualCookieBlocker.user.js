155110ff7402024475a0ceb09a059df0fe01c381
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-07-01 17:48:37 +0200

    Update ManualCookieBlocker.user.js

--- userscripts/ManualCookieBlocker.user.js
+++ userscripts/ManualCookieBlocker.user.js
@@ -2,7 +2,7 @@
 // @name         # Manual Cookie Blocker
 // @namespace    spsManualCookieBlocker
 // @description  Lot's of sites missing in Firefox's native blocker and I still don't care about cookies
-// @version      1.0.2
+// @version      1.0.3
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
@@ -24,133 +24,46 @@
 
 'use strict';
 var myRuns = 0;
-function gensokyo1() {
+function clicker(myStr) {
     myRuns++;
-    var button = document.querySelectorAll('button[id="sp-cc-rejectall-link"]');
-    if (button[0].length !== 0) {
+    console.log("Cookie clicker active:", myRuns)
+    var button = document.querySelectorAll(myStr);
+    if (typeof button[0] !== 'undefined') {
         button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
+    }
+    if (myRuns < 4) {
+        setTimeout(() => {
+            clicker(myStr);
+        }, 400);
     }
 }
 if (/https:\/\/www\.amazon\.[a-z]{2,3}\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId1 = setInterval(gensokyo1, 400);
-}
-function gensokyo2() {
-    myRuns++;
-    var button = document.querySelectorAll('button[class="accept cookie-notifications-btn"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('button[id="sp-cc-rejectall-link"]');
 }
 if (/https:\/\/hackaday\.com\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId2 = setInterval(gensokyo2, 400);
-}
-function gensokyo3() {
-    myRuns++;
-    var button = document.querySelectorAll('button[id="popin_tc_privacy_button_2"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('button[class="accept cookie-notifications-btn"]');
 }
 if (/https:\/\/www\.edeka\.de\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId3 = setInterval(gensokyo3, 400);
-}
-function gensokyo4() {
-    myRuns++;
-    var button = document.querySelectorAll('a[onclick="declineAnalytics();"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('button[id="popin_tc_privacy_button_2"]');
 }
 if (/https:\/\/rule34\.xxx\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId4 = setInterval(gensokyo4, 400);
-}
-function gensokyo5() {
-    myRuns++;
-    var button = document.querySelectorAll('a[onclick="tnc_agree();"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('a[onclick="declineAnalytics();"]');
 }
 if (/https:\/\/rule34\.paheal\.net\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId5 = setInterval(gensokyo5, 400);
-}
-function gensokyo6() {
-    myRuns++;
-    var button = document.querySelectorAll('a[onclick="declineAnalytics();"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('a[onclick="tnc_agree();"]');
 }
 if (/https:\/\/xbooru\.com\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId6 = setInterval(gensokyo6, 400);
-}
-function gensokyo7() {
-    myRuns++;
-    var button = document.querySelectorAll('div[data-tracking-opt-in-accept="true"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('a[onclick="declineAnalytics();"]');
 }
 if (/https:\/\/.*\.fandom\.com\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId7 = setInterval(gensokyo7, 400);
-}
-function gensokyo8() {
-    myRuns++;
-    var button = document.querySelectorAll('button[data-testid="reject-button"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('div[data-tracking-opt-in-accept="true"]');
 }
 if (/https:\/\/www\.imdb\.com\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId8 = setInterval(gensokyo8, 400);
-}
-function gensokyo9() {
-    myRuns++;
-    var button = document.querySelectorAll('a[class="cookie-permission--accept-button btn is--primary is--large is--center"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('button[data-testid="reject-button"]');
 }
 if (/https:\/\/www\.berrybase\.de\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId9 = setInterval(gensokyo9, 400);
-}
-function gensokyo10() {
-    myRuns++;
-    var button = document.querySelectorAll('a[class="cc-btn cc-dismiss"]');
-    if (button[0].length !== 0) {
-        button[0].click();
-    if (myRuns > 3) {
-            clearInterval(refreshIntervalId1);
-        }
-    }
+    clicker('a[class="cookie-permission--accept-button btn is--primary is--large is--center"]');
 }
 if (/https:\/\/clonezilla\.org\/.*/.test(window.location.href) == true) {
-    var refreshIntervalId10 = setInterval(gensokyo10, 400);
+    clicker('a[class="cc-btn cc-dismiss"]');
 }
