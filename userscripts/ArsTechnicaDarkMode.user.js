76a63a6220b295c0518a1bce8ec5938f6894497a
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-10-03 10:04:53 +0200

    Update ArsTechnicaDarkMode.user.js

--- userscripts/ArsTechnicaDarkMode.user.js
+++ userscripts/ArsTechnicaDarkMode.user.js
@@ -2,7 +2,7 @@
 // @name         # ArsTechnica Dark Mode
 // @namespace    spsArsTechnicaDarkMode
 // @description  Automatically turn on dark mode on Ars
-// @version      1.0.1
+// @version      1.0.2
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
@@ -17,6 +17,7 @@
 'use strict';
 if (document.cookie.includes('xf_style_id=5') == false){
     document.cookie = "xf_style_id=5; path=/";
+    document.cookie = "view=list; path=.";
     location.reload();
     console.log("this only runs once per session");
 }})();
