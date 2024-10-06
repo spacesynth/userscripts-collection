2f3d4ccf74c64981f327352e8ddadeb204fcff1a
spacesynth <64961250+spacesynth@users.noreply.github.com>
2024-10-06 13:09:52 +0200

    Update GoogleAutoDisableSafeSearch.user.js

--- userscripts/GoogleAutoDisableSafeSearch.user.js
+++ userscripts/GoogleAutoDisableSafeSearch.user.js
@@ -7,7 +7,7 @@
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
 // @license      WTFPL
-// @match        http*://www.google.*/search*
+// @include      *://*.google.tld/search*
 // @require      https://code.jquery.com/jquery-1.7.2.min.js
 // @require      https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/waitForKeyElements.js
 // @run-at       document-start
