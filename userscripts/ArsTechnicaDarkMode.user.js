ef9c35e75e87190601c48e7fc688b3b89f25448d
spacesynth <64961250+spacesynth@users.noreply.github.com>
2025-06-09 16:04:35 +0200

    Improved version for this week's change

--- userscripts/ArsTechnicaDarkMode.user.js
+++ userscripts/ArsTechnicaDarkMode.user.js
@@ -2,15 +2,14 @@
 // @name         # ArsTechnica Dark Mode
 // @namespace    spsArsTechnicaDarkMode
 // @description  Automatically turn on dark mode on Ars
-// @version      1.0.4
+// @version      1.0.5
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
 // @license      WTFPL
 // @match        *://*.arstechnica.com/*
 // @run-at       document-end
-// @grant        GM_cookie
-// @grant        GM.cookie
+// @grant        none
 // @noframes
 // ==/UserScript==
 
@@ -18,32 +17,7 @@
 'use strict';
 if (document.cookie.includes('theme=dark') == false){
     document.cookie = "theme=dark; path=/";
-GM_cookie.set(
-    {
-        domain: ".arstechnica.com",
-        //hostOnly: true,
-        httpOnly: true,
-        name: "view",
-        //path: ".",
-        //sameSite: "no_restriction",
-        secure: true,
-        session: false,
-        value: "list"
-    }
-)
-GM_cookie.set(
-    {
-        domain: ".arstechnica.com",
-        //hostOnly: true,
-        httpOnly: true,
-        name: "fw_view",
-        //path: ".",
-        //sameSite: "no_restriction",
-        secure: true,
-        session: false,
-        value: "off"
-    }
-)
+    document.cookie = "view=list; domain=.arstechnica.com";
+    document.cookie = "fw_view=off; domain=.arstechnica.com";
     location.reload();
-    console.log("this only runs once per session");
 }})();
