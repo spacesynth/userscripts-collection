f2ec962788f0ea66e51aeac823ab6853d9fde843
spacesynth <64961250+spacesynth@users.noreply.github.com>
2025-06-13 20:25:26 +0200

    Stop getting on my arse, ars

--- userscripts/ArsTechnicaDarkMode.user.js
+++ userscripts/ArsTechnicaDarkMode.user.js
@@ -2,14 +2,15 @@
 // @name         # ArsTechnica Dark Mode
 // @namespace    spsArsTechnicaDarkMode
 // @description  Automatically turn on dark mode on Ars
-// @version      1.0.5
+// @version      1.0.6
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
 // @license      WTFPL
 // @match        *://*.arstechnica.com/*
 // @run-at       document-end
-// @grant        none
+// @grant        GM_cookie
+// @grant        GM.cookie
 // @noframes
 // ==/UserScript==
 
@@ -17,7 +18,35 @@
 'use strict';
 if (document.cookie.includes('theme=dark') == false){
     document.cookie = "theme=dark; path=/";
-    document.cookie = "view=list; domain=.arstechnica.com";
-    document.cookie = "fw_view=off; domain=.arstechnica.com";
+    //document.cookie = "view=list; domain=.arstechnica.com";
+    //document.cookie = "fw_view=off; domain=.arstechnica.com";
+GM.cookie.delete({ name: 'fw_view' })
+GM_cookie.set(
+    {
+        domain: ".arstechnica.com",
+        hostOnly: true,
+        httpOnly: false,
+        name: "fw_view",
+        path: "/",
+        sameSite: "no_restriction",
+        secure: true,
+        session: false,
+        value: "off"
+    }
+)
+GM.cookie.delete({ name: 'view' })
+GM_cookie.set(
+    {
+        domain: ".arstechnica.com",
+        hostOnly: true,
+        httpOnly: false,
+        name: "fw_view",
+        path: "/",
+        sameSite: "no_restriction",
+        secure: true,
+        session: false,
+        value: "list"
+    }
+)
     location.reload();
 }})();
