4404dd6c3f7ff77a42297841647804e2c518c01d
spacesynth <64961250+spacesynth@users.noreply.github.com>
2025-07-21 13:44:54 +0200

    Update YouTubeNoMiniplayer.user.js

--- userscripts/YouTubeNoMiniplayer.user.js
+++ userscripts/YouTubeNoMiniplayer.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         # YouTube No Miniplayer
+// @name         # YouTube No Miniplayer + Cookie refuser
 // @namespace    spsYouTubeNoMiniplayer
 // @description  No miniplayer on YouTube
-// @version      1.0.4
+// @version      1.0.5
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
+// @require      https://code.jquery.com/jquery-1.7.2.min.js
+// @require      https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/waitForKeyElements.js
 // @license      WTFPL
 // @match        *://*.youtube.com/*
 // @run-at       document-start
@@ -22,4 +24,12 @@ function clicker() {
         }
     }
 }
+function gensokyo() {
+    var element1 = document.querySelector('button[aria-label="Reject the use of cookies and other data for the purposes described"]');
+    if (element1 != undefined) {
+        element1.click();
+    }
+    return true;
+}
+waitForKeyElements('button[aria-label="Reject the use of cookies and other data for the purposes described"]', gensokyo);
 var refreshIntervalId4 = setInterval(clicker, 400);
