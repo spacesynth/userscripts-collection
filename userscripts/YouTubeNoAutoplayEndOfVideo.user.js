33c74d49d9ac8122a6e8d7a8023d75adc777fdcf
spacesynth <i9100@arcor.de>
2026-03-16 15:30:26 +0100

    Initial commit

---
+++ userscripts/YouTubeNoAutoplayEndOfVideo.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         # YouTube No Autoplay (End of Video)
+// @namespace    spsYouTubeNoAutoplayEndOfVideo
+// @description  Check if the autoplay crap sneaked back in
+// @version      1.0.0
+// @author       spacesynth
+// @supportURL   https://github.com/spacesynth/userscripts-collection
+// @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
+// @require      https://code.jquery.com/jquery-1.7.2.min.js
+// @require      https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/waitForKeyElements.js
+// @license      WTFPL
+// @match        *://*.youtube.com/watch*
+// @run-at       document-idle
+// @noframes
+// ==/UserScript==
+
+'use strict';
+var refreshIntervalId4;
+function force_reject_all() {
+    var element1 = document.querySelector('button[aria-label="Cancel autoplay"]');
+    if ($(element1).is(':visible') == true) {
+        setTimeout(function(){
+        element1 && element1.click();
+        }, 1000);
+    }
+    return true;
+}
+waitForKeyElements('button[aria-label="Cancel autoplay"]', refreshIntervalId4 = setInterval(force_reject_all, 1000));
