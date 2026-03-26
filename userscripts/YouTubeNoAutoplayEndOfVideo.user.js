fad6959e0e05f0eabcb395839f06f1e69356b33a
STS-99 <64961250+spacesynth@users.noreply.github.com>
2026-03-26 09:15:55 +0100

    internationalize

--- userscripts/YouTubeNoAutoplayEndOfVideo.user.js
+++ userscripts/YouTubeNoAutoplayEndOfVideo.user.js
@@ -2,7 +2,7 @@
 // @name         # YouTube No Autoplay (End of Video)
 // @namespace    spsYouTubeNoAutoplayEndOfVideo
 // @description  Check if the autoplay crap sneaked back in
-// @version      1.0.0
+// @version      1.0.1
 // @author       spacesynth
 // @supportURL   https://github.com/spacesynth/userscripts-collection
 // @icon         https://raw.githubusercontent.com/spacesynth/userscripts-collection/master/utility/icon.png
@@ -17,7 +17,7 @@
 'use strict';
 var refreshIntervalId4;
 function force_reject_all() {
-    var element1 = document.querySelector('button[aria-label="Cancel autoplay"]');
+    var element1 = document.querySelector('button[class^="ytp-autonav-endscreen-upnext-button"]');
     if ($(element1).is(':visible') == true) {
         setTimeout(function(){
         element1 && element1.click();
@@ -25,4 +25,4 @@ function force_reject_all() {
     }
     return true;
 }
-waitForKeyElements('button[aria-label="Cancel autoplay"]', refreshIntervalId4 = setInterval(force_reject_all, 1000));
+waitForKeyElements('button[class^="ytp-autonav-endscreen-upnext-button"]', refreshIntervalId4 = setInterval(force_reject_all, 1000));
